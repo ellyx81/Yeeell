2e55316fead133cbdd9c572bf70756744cf2d0fe
giselle <gisellesan0801@gmail.com>
2023-06-05 09:28:57 +0800

    created FAQ section

--- src/js/top/index.ts
+++ src/js/top/index.ts
@@ -1,9 +1,11 @@
+import accordion from './components/accordion'
 import slider from './components/slider'
 
 document.addEventListener(
   'DOMContentLoaded',
   () => {
     slider()
+    accordion()
   },
   false
 )
